cb73fb6f15d4e83b28c7971541944b36703d9a44
Neelesh Thakur <nt00605@gmail.com>
2020-12-30 13:06:49 +0530

    added routes in constants folder and added react routering in app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import FaqsContainer from './containers/faqs.js';
 import FooterContainer from './containers/footer.js';
 import JumbotronContainer from './containers/jumbotron.js';
+import ROUTES from './constants/routes';
 
 function App() {
   return (
-      <>
-        <JumbotronContainer/>
-        <FaqsContainer />
+      <BrowserRouter>
+        <Switch>
+        <Route exact path={ROUTES.HOME}><JumbotronContainer/></Route>
+        <Route exact path={ROUTES.BROWSE}><FaqsContainer /></Route>
+        </Switch>
         <FooterContainer />
-      </>
+      </BrowserRouter>
   );
 }
 
---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+const routes = {
+    HOME: '/',
+    BROWSE: '/browse',
+    SIGN_UP: '/signup',
+    SIGN_IN: '/signin',
+}
+export default routes;
+
