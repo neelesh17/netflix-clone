2570a8b8fb6504888baa980d21f25e1426a8f68b
Neelesh Thakur <nt00605@gmail.com>
2020-12-30 11:29:10 +0530

    added fotter styles

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Container, Row, Column, Link, Title, Text, Break} from './styles/footer';
 
-function Footer() {
+function Footer({children, ...restProps}) {
     return (
-        <div>
-            
-        </div>
+        <Container {...restProps}>{children}</Container>
     )
 }
 
+Footer.Row = function FooterRow({children, ...restProps}){
+    return <Row {...restProps}>{children}</Row>
+}
+
+Footer.Column = function FooterColumn({children, ...restProps}){
+    return <Column {...restProps}>{children}</Column>
+}
+
+Footer.Link = function FooterLink({children, ...restProps}){
+    return <Link {...restProps}>{children}</Link>
+}
+
+Footer.Title = function FooterTitle({children, ...restProps}){
+    return <Title {...restProps}>{children}</Title>
+}
 export default Footer
